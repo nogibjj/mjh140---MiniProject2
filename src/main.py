b480a1659b89bd9c662017e524218d33569a2c3b
Matthew Holden <114833075+matthold86@users.noreply.github.com>
2023-09-06 14:47:58 -0400

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+def printfunc(x : str) -> str:
+    return print(x)
+
+if __name__ == "__main__":
+    printfunc("Hello World!")
---
+++ tests/tests.py
@@ -0,0 +1,4 @@
+from src.main import printfunc as pf
+
+def testprint():
+    assert pf("String of text") == "String of text"
