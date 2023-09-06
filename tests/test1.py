020d3fe7a9eca50c78c89f4719d2de8c0a72ecc8
Matthew Holden <matthew.holden86@gmail.com>
2023-09-06 18:52:04 -0400

    changed test name

--- tests/test1.py
+++ tests/test1.py
@@ -1,7 +1,7 @@
 from src.main import printfunc as pf
 
-def testprint():
+def test_print():
     assert pf("String of text") == "String of text"
 
 if __name__ == "__main__":
-    testprint()
+    test_print()
